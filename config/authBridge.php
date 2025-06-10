296900283b5ceb3e98a1c790cca9460897b81a9d
Stephen Luck Enguio <stephenenguio1120@gmail.com>
2025-06-10 14:22:37 +0800

    chore: fixed typos

--- config/authBridge.php
+++ config/authBridge.php
@@ -2,10 +2,10 @@
 
 return [
     'oauth' => [
-        'api_base' => env('AUTHBRIDGE_API_BASE'),
+        'api_base' => env('AUTHBRIDGE_OAUTH_API_BASE'),
     ],
 
     'clinic' => [
-        'api_base' => env('AUTHBRIDGE_API_BASE'),
+        'api_base' => env('AUTHBRIDGE_CLINIC_API_BASE'),
     ],
 ];
